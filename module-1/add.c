9af48a997324ca81decfdbb7e15ae24b2b615ac3
enigma0884 <sohanshashi0884@gmail.com>
2022-05-12 18:29:45 +0530

    add a few basic programs

---
+++ module-1/add.c
@@ -0,0 +1,16 @@
+/*
+    Authored by Sohan Shashikumar
+    CSE Dept, DSCE.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 420;
+    int b = 69;
+
+    printf("The sum of %d and %d is: %d", a, b, a + b);
+    return 0;
+}
